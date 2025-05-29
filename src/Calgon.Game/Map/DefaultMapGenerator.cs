38d60497652b3f98dc67657e98c12a85d03471bc
Konstantin Vasilkovsky <quiphop@gmail.com>
2025-05-29 14:05:56 +0300

    Built-ins cleared and replaced

--- src/Calgon.Game/Map/DefaultMapGenerator.cs
+++ src/Calgon.Game/Map/DefaultMapGenerator.cs
@@ -16,8 +16,8 @@ internal sealed class DefaultMapGenerator : IMapGenerator
     {
         var cellSize = MapSize / GridSize;
 
-        var zoneIndices = Enumerable.Range(0, GridSize * GridSize).ToList();
-        Shuffle(zoneIndices); // Shuffle "zones"
+        var zoneIndices = Enumerable.Range(0, GridSize * GridSize).ToArray();
+        Random.Shuffle(zoneIndices); // Use built-in shuffle
 
         var planets = new List<Planet>(capacity: PlanetsCapacity);
 
@@ -38,37 +38,17 @@ internal sealed class DefaultMapGenerator : IMapGenerator
             var offsetX = Random.Next(-cellSize / 4, cellSize / 4);
             var offsetY = Random.Next(-cellSize / 4, cellSize / 4);
 
-            var x = Clamp(baseX + offsetX, size, MapSize - size);
-            var y = Clamp(baseY + offsetY, size, MapSize - size);
+            var x = Math.Clamp(baseX + offsetX, size, MapSize - size);
+            var y = Math.Clamp(baseY + offsetY, size, MapSize - size);
 
             var location = new Location(x, y);
 
-            // Check distance between planets
-            if (planets.All(p => Distance(p.Location, location) >= MinDistance + size + p.Size))
+            if (planets.All(p => p.Location.DistanceTo(location) >= MinDistance + size + p.Size))
             {
                 planets.Add(new Planet(location, size));
             }
         }
 
         return new Map(MapSize, planets);
-    }
-
-    private static void Shuffle<T>(IList<T> list)
-    {
-        for (int i = list.Count - 1; i > 0; i--)
-        {
-            int j = Random.Next(i + 1);
-            (list[i], list[j]) = (list[j], list[i]);
-        }
-    }
-
-    private static int Clamp(int value, int min, int max)
-        => Math.Min(Math.Max(value, min), max);
-
-    private static double Distance(Location a, Location b)
-    {
-        var dx = a.X - b.X;
-        var dy = a.Y - b.Y;
-        return Math.Sqrt(dx * dx + dy * dy);
     }
 }
