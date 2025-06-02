5b3dbd442f52fbf11afd61fbd540bd93580fd578
Vladyslav Kalashnykov-Travin <16034470+inayelle@users.noreply.github.com>
2025-06-02 12:01:24 +0300

    Create the PlayerStats entity

---
+++ src/Calgon.Host/Data/Entities/PlayerStats.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Calgon.Host.Data.Entities;
+
+public sealed class PlayerStats
+{
+    [Key]
+    public Guid PlayerId { get; private init; }
+
+    public long TotalGames { get; private set; }
+    public long WonGames { get; private set; }
+
+    private PlayerStats()
+    {
+    }
+
+    public static PlayerStats Create(Guid playerId, bool didWinFirstGame)
+    {
+        return new PlayerStats
+        {
+            PlayerId = playerId,
+            TotalGames = 1,
+            WonGames = didWinFirstGame ? 0 : 1,
+        };
+    }
+
+    public void AddGame(bool playerIsWinner)
+    {
+        TotalGames++;
+
+        if (playerIsWinner)
+        {
+            WonGames++;
+        }
+    }
+}
--- src/Calgon.Host/Migrations/20250602090103_Add_PlayerStats.Designer.cs
+++ src/Calgon.Host/Migrations/20250602090103_Add_PlayerStats.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Calgon.Host.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Calgon.Host.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250602090103_Add_PlayerStats")]
+    partial class Add_PlayerStats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
